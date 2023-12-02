f7fe31ad68092f155cd160f6a5a25bda0a512585
isicheri <tech2dom@gmail.com>
2023-12-02 20:09:52 +0100

    added few changes

--- index.js
+++ index.js
@@ -6,6 +6,37 @@ const threeBar = document.querySelector('.three')
 const the_animation = document.querySelectorAll('.animator')
 
 
+
+window.onload = function () {
+    let slides = document.getElementsByClassName('carousel-item')
+
+    function addActive(slide) {
+        slide.classList.add('active')
+    }
+
+    function removeActive(slide) {
+        slide.classList.remove('active')
+    }
+
+    addActive(slides[0])
+    setInterval(() => {
+        for (let i = 0; i < slides.length; i++) {
+            if (i + 1 == slides.length) {
+                addActive(slides[0])
+                setTimeout(removeActive,350,slides[i])
+                break
+            } 
+
+            if(slides[i].classList.contains('active')) {
+                setTimeout(removeActive,350,slides[i])
+                addActive(slides[i+1])
+                break
+            }
+        }
+    },1500)
+
+}
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if(entry.isIntersecting) {
