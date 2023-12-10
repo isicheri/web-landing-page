e00b1ff8e741a0e9b0410c7aabcbfc3bfe4afa39
isicheri <tech2dom@gmail.com>
2023-12-10 03:02:53 +0100

    this is another update

--- index.js
+++ index.js
@@ -61,3 +61,22 @@ bar.addEventListener('click',() => {
     twoBar.classList.toggle('c')
    threeBar.classList.toggle('b')
 })
+
+let mySwiper = new Swiper('.container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    autoplay: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 1,
+        depth: 100,
+        modifier: 7
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    }
+});
